0def54632cdfd0ca6ca619b618ad9694dc8518b4
Dicle Affazoglu <affazogludicle@gmail.com>
2023-12-26 14:40:26 +0200

    End of section 2

---
+++ Domain/Activity.cs
@@ -0,0 +1,14 @@
+
+namespace Domain
+{
+    public class Activity
+    {
+        public Guid Id { get; set; }
+        public String Title { get; set; }
+        public DateTime Date { get; set; }
+        public string Description { get; set; }
+        public string Category { get; set; }
+        public string City { get; set; }
+        public string Venue { get; set; }
+    }
+}
